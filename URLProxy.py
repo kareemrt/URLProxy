cb1d985b0a6e5ffddc7eb901b390a1ea3b2b1e35
Kareemrt <kareemrt@gmail.com>
2023-12-06 00:08:36 -0800

    bad_ip feature cleanup

--- URLProxy.py
+++ URLProxy.py
@@ -1,48 +1,66 @@
 # Name : URLProxy.py
 # Auth : KT
 # Desc : Implimentation of rotating proxy functionality on top of the requests module
-import json
-import random
-import time
-from requests import Session
 
-session = Session()
+import json                     # User credential file
+import random                   # Random proxy generation
+import time                     # Pause between requests
+from requests import Session    # Session
 
-def read_credentials(file_path = 'IO/credentials.json'):
+session = Session() # Maintain single session to (substantially) increase sequential request speed 
+
+def socks_credentials(file_path = 'IO/credentials.json'):
     '''Reads JSON file to load alternative HTML headers, Socks5 proxies, & proxy credentials (user:pass)'''
 
+    # Open credentials file at specified filepath (default: './IO/credentials.json')
     with open(file_path, "r") as c:
         data = json.load(c)
-        username, password, credentials = data['credentials']
-        headers = [header for header in data['headers']]
-        proxies = [proxy for proxy in data['proxies']]
+        username, password, credentials = data['credentials']   # Socks5 proxy credentials
+        headers = [header for header in data['headers']]        # Alternative browser headers
+        proxies = [proxy for proxy in data['proxies']]          # Alternative proxies
     return username, password, credentials, proxies, headers
 
-user, passw, credentials, proxies, headers = read_credentials()
+user, passw, credentials, IPs, headers = socks_credentials()
+Bad_IPs = {}
 
-def force_connect(url):
+def force_connect(url, timeout = 5):
     '''Returns a web request using randomly-chosen Socks5 proxies until 1 works'''
 
-    IP, agent = random.choice(proxies), random.choice(headers) # Random IP, HTML Header
-    proxy = "socks5h://" + credentials + '@' + IP + ":1080"    # Combine
-    broken_proxies = {}
-    url_req = -1
+    # Instantiate variables
+    global IPs, Bad_IPs                                         # Manage IP's outside of function scope
+    IP, agent = random.choice(IPs), random.choice(headers)      # Random IP, HTML Header
+    proxy = "socks5h://" + credentials + '@' + IP + ":1080"     # Combine
 
-    while url_req == -1:
-        try: url_req = session.get(url, proxies={'http':proxy, 'https':proxy}, headers={'User-Agent':agent}, timeout=10) # Use random info
+    # Try connection
+    r = -1
+    while r == -1:
+        try: r = session.get(url, proxies={'http':proxy, 'https':proxy}, headers={'User-Agent':agent}, timeout=10) # Request
         except Exception: 
-            print(f"Connection Error via proxy: {IP}")
-            if not broken_proxies.get(IP): 
-                broken_proxies[IP] = 0
-            # Remove proxy if broken 5 times
-            broken_proxies[IP] += 1
-            if broken_proxies.get(IP) == 5: 
-                proxies.remove(IP)
-                if len(proxies) == 0: proxies = list(broken_proxies.keys())
+            
+            # Connection failure
+            print(f"Error: Connection failed via proxy: {IP}")
+
+            # Add broken IP to list
+            if not Bad_IPs.get(IP, None): 
+                print(f'{IP} -> Bad_IPs dict')
+                Bad_IPs[IP] = 0
+
+            # Delete broken proxies that meet timeout threshold
+            Bad_IPs[IP] += 1
+            if Bad_IPs.get(IP) == timeout: 
+                print(f'Deleting {IP} from working IP dict')
+                IPs.remove(IP)
+
+            # All proxes have timed out
+            if len(IPs) == 0:
+                print('All proxies have timed out, clearing Bad_IPs cache')
+                IPs = list(Bad_IPs.keys())
+
             # Refresh new proxy
-            IP, agent = random.choice(proxies), random.choice(headers) # Cycle proxy if connection fails
+            IP, agent = random.choice(IPs), random.choice(headers)
             proxy = "socks5h://" + credentials + '@' + IP + ":1080"
             time.sleep(1)
 
+    # Connection success
     print(f'Successful connection via proxy: {IP}')
-    return url_req
+    return r
